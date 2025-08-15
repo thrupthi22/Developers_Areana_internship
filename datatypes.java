215e49a7e783ade386f79f75c77c0de58ae1db22
Thrupthi S <153356024+thrupthi22@users.noreply.github.com>
2025-08-15 15:26:40 +0530

    different datatypes

---
+++ datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // Integer type
+        int age = 20;
+        // Floating-point type
+        double price = 99.99;
+        // Character type
+        char grade = 'A';
+        // Boolean type
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
